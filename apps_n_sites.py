98499d1beceeefd2e7b88e8ed4e8a0c078ad5f10
amwanjesa <a.mwanjesa@gmail.com>
2018-09-22 22:58:18 +0200

    Apps n Websites, simple setup

--- apps_n_sites.py
+++ apps_n_sites.py
@@ -3,24 +3,27 @@ import click
 import json
 
 
+class Apps_n_Sites:
+    def __init__(self, data_file):
+        with open(data_file, 'r') as f:
+            data = json.load(f)
+        self.records = pd.DataFrame(data['installed_apps'])
+        self.records["added_timestamp"] = pd.to_datetime(
+            self.records['added_timestamp'], unit='s')
+        self.records.set_index('added_timestamp', inplace=True)
+
+    def get_year(self, year):
+        return self.records[(self.records.index >= '{}-1-1'.format(year)) &
+                            (self.records.index <= '{}-12-31'.format(year))]
+
+
 @click.command()
 @click.argument('data_file')
 def main(data_file):
-    with open(data_file, 'r') as f:
-        data = json.load(f)
-    print(data)
-    df = pd.DataFrame(data['installed_apps'])
-    df["added_timestamp"] = pd.to_datetime(df['added_timestamp'], unit='s')
-    df.set_index('added_timestamp', inplace=True)
+    apps = Apps_n_Sites(data_file)
 
-    df_11 = get_year(df, 2012)
-    import pdb
-    pdb.set_trace()
-
-
-def get_year(df, year):
-    return df[(df.index >= '{}-1-1'.format(year)) &
-              (df.index <= '{}-12-31'.format(year))]
+    df_11 = apps.get_year(2012)
+    print(df_11)
 
 
 if __name__ == '__main__':
